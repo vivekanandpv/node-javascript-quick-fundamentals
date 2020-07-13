9797b77bef3ab6a565a810dcbd932098f8258818
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-13 22:04:40 +0530

    done

--- example.js
+++ example.js
@@ -1,36 +1,23 @@
-//  A pure function always returns the same output for same input
-//  It is stateless, meaning it doesn't keep a memory of variables.
-//  It doesn't produce side-effects (somewhere else)
-//  More info: https://en.wikipedia.org/wiki/Pure_function
+//  A higher-order function can:
+//  a. take another function as an argument
+//  b. return a function as the return value
+//  c. do both a & b
 
-let i = 0;
-
-//  square() only depends on input parameter and constants
-//  It doesn't change or call anything else
-//  So it is a pure function
-function square(param) {
-  return param * param;
+function upperCaseConverter(s) {
+  return s.toUpperCase();
 }
 
-//  foo() doesn't depend only on param
-//  Also, it changes external variable i
-//  This is not a pure function
-function foo(param) {
-  ++i;
-  return param * i;
+function fooTakesFunctionArgument(fn) {
+  console.log(`fooTakesFunctionArgument calls fn: ${fn('hello')}`);
 }
 
-//  Calling square with same input,
-//  say 3 always gives the same result 9
-console.log(`square(3): ${square(3)}`);
-console.log(`square(3): ${square(3)}`);
-console.log(`square(3): ${square(3)}`);
-console.log(`square(3): ${square(3)}`);
+fooTakesFunctionArgument(upperCaseConverter);
 
-//  On the other hand, calling foo with same input,
-//  say 3 doesn't always give the same result
-console.log(`foo(3): ${foo(3)}`);
-console.log(`foo(3): ${foo(3)}`);
-console.log(`foo(3): ${foo(3)}`);
-console.log(`foo(3): ${foo(3)}`);
-console.log(`foo(3): ${foo(3)}`);
+function fooReturnsAFunction() {
+  return function () {
+    console.log('return function of fooReturnsAFunction()');
+  };
+}
+
+let returnFunction = fooReturnsAFunction();
+returnFunction();
