698e950fab4a8c374fc92abdd6b7a214759b62a5
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-13 21:46:59 +0530

    done

--- example.js
+++ example.js
@@ -1,8 +1,40 @@
-//  1. using concatenation with empty string
-let i = '' + 1002;
-console.log(`i: ${i}, type of i: ${typeof i}`);
+//  In its simple form, an object in JavaScript is
+//  a set of key-value pairs. Keys are called as properties.
+//  Each property has to be unique. Value can be of any type,
+//  including another object (nested object), or an array,
+//  or any combination thereof.
 
-//  2. using Number.toString()
-let j = 1500;
-let stringJ = j.toString();
-console.log(`stringJ: ${stringJ}, type of stringJ: ${typeof stringJ}`);
+//  An example
+
+let book = {
+  title: 'Anna Karenina',
+  year: 1878,
+  author: {
+    firstName: 'Leo',
+    lastName: 'Tolstoy',
+    country: 'Russia',
+  },
+  translatedTo: ['English', 'French', 'German'],
+  isAvailable: true,
+  order: function () {
+    console.log('Order taken for Anna Karenina');
+  },
+};
+
+//  Properties are accessed via dot operator
+console.log(`author first name: ${book.author.firstName}`);
+
+//  Function on the object can be invoked using dot operator
+book.order();
+
+//  Properties can also be accessed using subscript operator
+console.log(`title: ${book['title']}`);
+book['order']();
+
+//  A new property can be added on the fly
+book.language = 'Russian';
+console.log(`language: ${book.language}`);
+
+//  An existing property can be deleted on the fly
+delete book.year;
+console.log('Book after deleting year property', book);
