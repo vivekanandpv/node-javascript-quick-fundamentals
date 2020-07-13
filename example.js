504524035dd5f65bd0f1897cae66561738c89a76
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-13 22:46:19 +0530

    done

--- example.js
+++ example.js
@@ -1,15 +1,18 @@
+console.log(`Module executing scope this: ${this}`);
+
 const book = {
   title: 'JavaScript the Good Parts',
-  read: function () {
+  read: () => {
+    //    read() now referes this to it's enclosing 'executing' scope
+    //    please note, the object book is not an 'executing' scope
+    //    the executing scope is the module (check line 1)
+    //    thus, this.title is same as writing this.title in line 1 (will be undefined)
     console.log(`Title: ${this.title}`);
   },
 };
 
-//  if read() is called on book, this refers to the book
+//  since read() is now an arrow function, its behaviour is consistent
 book.read();
 
-//  if read() is called independently, this refers to the global object
-//  hence this.title is undefined
-
-const readFunction = book.read; //  this is assignment
+const readFunction = book.read;
 readFunction();
