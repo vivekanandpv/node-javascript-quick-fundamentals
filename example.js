3b9a2f69faceee0a1a992ca89608a0b0d5f4af7b
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-13 22:08:54 +0530

    done

--- example.js
+++ example.js
@@ -1,23 +1,19 @@
-//  A higher-order function can:
-//  a. take another function as an argument
-//  b. return a function as the return value
-//  c. do both a & b
+//  When a higher order function (say foo) returns a function
+//  this return function closes over the local variables
+//  declared in the foo() scope. The return function
+//  has the access to these varialbes, even after foo() is out of scope
 
-function upperCaseConverter(s) {
-  return s.toUpperCase();
-}
-
-function fooTakesFunctionArgument(fn) {
-  console.log(`fooTakesFunctionArgument calls fn: ${fn('hello')}`);
-}
-
-fooTakesFunctionArgument(upperCaseConverter);
-
-function fooReturnsAFunction() {
+function closureEnclosureFunction() {
+  let counter = 0;
   return function () {
-    console.log('return function of fooReturnsAFunction()');
+    ++counter;
+    console.log(`Current counter is: ${counter}`);
   };
 }
 
-let returnFunction = fooReturnsAFunction();
-returnFunction();
+let returnFunction = closureEnclosureFunction(); //  here, closureEnclosureFunction is out of scope
+
+returnFunction(); //  counter 1
+returnFunction(); //  counter 2
+returnFunction(); //  counter 3
+returnFunction(); //  counter 4
